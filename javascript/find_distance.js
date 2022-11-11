fb769119867cccc8cae152d4f6d7abe1c48718f9
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-11 10:13:55 -0500

    done with assignment

--- javascript/find_distance.js
+++ javascript/find_distance.js
@@ -1,5 +1,26 @@
 function findDistance(graph, vertexA, vertexB) {
-  // type your code here
+  const queue = graph[vertexA] ? [graph[vertexA]] : [];
+	const visited = new Set(vertexA);
+
+	let distance = 1;
+
+	while (queue.length > 0) {
+		const arr = queue.shift();
+		if (arr.includes(vertexB)) {
+			return distance;
+		} else {
+			if (queue.length === 0) {
+				distance++;
+			}
+			arr.forEach(vertex => {
+				if (!visited.has(vertex)) {
+					visited.add(vertex);
+					queue.push(graph[vertex]);
+				}
+			});
+		}
+	}
+	return -1;
 }
 
 if (require.main === module) {
@@ -25,3 +46,18 @@ module.exports = findDistance;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/**
+ * Creae empty queue
+ * Create visited array
+ * Create distance count at 0
+ * Add vertexA to queue
+ * While queue is not empty:
+ * 	take first vertex from queue
+ * 	add vertex to visited
+ * 	if vertex is equal to target, return distance
+ * 	otherwise, iterate through adjacency array of vertex
+ * 		if element is not in visited set
+ * 			add element to queue
+ * 		increase distance
+ */
